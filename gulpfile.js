3b0a3fc3b00c38a68f2240cf491d5cf0dca4a419
bananenmannfrau <bananenmannfrau@posteo.net>
2017-05-17 21:02:50 +0200

    initial setup

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+    server: "./"
+  });
+
+  gulp.watch("scss/*.scss", ['sass']);
+  gulp.watch("*.html").on('change', browserSync.reload);
+  gulp.watch("js/*.js", ['js-watch']);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src("scss/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("dist/css"))
+    .pipe(browserSync.stream());
+});
+// process JS files and return the stream.
+gulp.task('js', function () {
+  return gulp.src('js/*js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
+
+// create a task that ensures the `js` task is complete before
+// reloading browsers
+gulp.task('js-watch', ['js'], function (done) {
+  browserSync.reload();
+  done();
+});
+
+gulp.task('default', ['serve']);
